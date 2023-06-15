78d45b508c90a7df3ec81a9af74a9488b5e96209
Toheeb Temitope <toheebtemitope5@gmail.com>
2023-06-15 11:00:25 -0700

    Hello, printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,9 +3,8 @@
  * main - A program that orint s a line using the printf function
  * Return: 0 (Success)
  */
-
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n")
-	return (0);
+printf("with proper grammar, but the outcome is a piece of art,\n")
+return (0);
 }
