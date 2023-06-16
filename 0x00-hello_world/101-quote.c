3b640ceb77f242d1bd9b22258af8e541774b5b3c
Toheeb Temitope <toheebtemitope5@gmail.com>
2023-06-16 07:07:24 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,11 @@
-#include <stdio.h>
 #include <unistd.h>
 /**
  * main - A c programe that prints a line to the standard error
- * Return 1 (Success)
+ * Return: 1 (Success)
  */
 int main(void)
 {
-	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",stdout);
+	write(2,
+		"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
 return (1);
 }
