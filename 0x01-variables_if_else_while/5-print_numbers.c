12354099534692f183e4ae70acba3f5e6804ed03
Toheeb Temitope <toheebtemitope5@gmail.com>
2023-06-19 05:23:51 -0700

    5-print_numbers.c

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
 /**
- * main - Prints numbers from 0 to 9
+ * main - Prints numbers from 0 to 9.
  *
- * Return Always 0 (Success)
+ * Return:  Always 0 (Success)
  */
 int main(void)
 {
