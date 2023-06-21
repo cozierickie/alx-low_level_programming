c13bdce0d4f36c04724d36d4b2901505222640ba
Toheeb Temitope <toheebtemitope5@gmail.com>
2023-06-21 08:00:22 -0700

    7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -11,10 +11,10 @@ int print_last_digit(int n)
 {
 	int last_digit;
 
-	if (n < 0)
-	n = -n;
-
 	last_digit = n % 10;
+	if (last_digit < 0)
+		last_digit = -last_digit;
+
 	_putchar('0' + last_digit);
 
 	return (last_digit);
