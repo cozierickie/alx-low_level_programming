4a61b42c8654c97dae8913b8db7597aa77fd8197
Toheeb Temitope <toheebtemitope5@gmail.com>
2023-06-21 13:42:40 -0700

    8-24_hours.c

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * jack_bauer - Prints every munite of the day of Jack Bauer
+ *
+ * Description: This function prints the time in the format HH:MM for every
+ * minute of the day, from 00:00 to 23:59.
+ */
+void jack_bauer(void)
+{
+	int hour, minute;
+
+	for (hour = 0; hour < 24; hour++)
+	{
+		for (minute = 0; minute < 60; minute++)
+		{
+			_putchar('0' + (hour / 10));
+			_putchar('0' + (hour % 10));
+			_putchar(':');
+			_putchar('0' + (minute / 10));
+			_putchar('0' + (minute % 10));
+			_putchar('\n');
+		}
+	}
+}
